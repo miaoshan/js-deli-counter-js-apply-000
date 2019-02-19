8729243b1eb0bf11126d9e73a72f347943260633
miao shan <minnieshan@gmail.com>
2019-02-19 22:09:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(currentLine){
 }
 function currentLine(line){
   var newline=[];
-  if(line==0){
+  if(line.length==0){
     return "The line is currently empty.";
   }
   else{
